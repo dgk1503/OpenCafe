2bc6db53e82644b6cbdeba31ed9be257b9d614a6
dgk <dkarthikeya2008@gmail.com>
2026-03-31 23:15:41 +0530

    LayOut Change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,47 +5,14 @@ import Navbar from "./components/Navbar";
 import Preloader from "./components/PreLoader";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import { Canvas } from "@react-three/fiber";
+
 gsap.registerPlugin(ScrollTrigger);
-import Scene from "./components/Scene";
+
 export default function Home() {
   const [loaded, SetLoaded] = useState(false);
   const mainRef = useRef(null);
-  const SceneRef = useRef(null);
+
   const contentRef = useRef(null);
-  const scrollProgress = useRef(0);
-
-  useEffect(() => {
-    if (!mainRef.current || !contentRef.current) return;
-
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: mainRef.current,
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 1,
-        pin: true,
-        onUpdate: (self) => {
-          scrollProgress.current = self.progress;
-        },
-      },
-    });
-
-    tl.to({}, { duration: 1 });
-
-    tl.to(SceneRef.current, {
-      filter: "blur(5px)",
-      scale: 1.05,
-      duration: 0.5,
-    });
-    tl.to(contentRef.current, {
-      opacity: 1,
-      y: 0,
-      pointerEvents: "auto",
-      duration: 0.6,
-      ease: "power3.out",
-    });
-  }, []);
 
   return (
     <div>
@@ -55,29 +22,18 @@ export default function Home() {
         </div>
       )}
       <div ref={mainRef} className="relative">
-        <div className="fixed top-0 left-0 h-screen w-full" ref={SceneRef}>
-          <Canvas frameloop="demand">
-            <Scene scrollProgress={scrollProgress} />
-          </Canvas>
-        </div>
+        <Navbar />
 
-        <div className="h-[200vh]" />
-
-        <div
-          ref={contentRef}
-          className="opacity-0 translate-y-10 pointer-events-none "
-        >
-          <Navbar />
-
+        <div className="h-[400vh]">
           <div className="mx-auto flex flex-col bg-amber-700 font-bold text-xl text-black rounded-4xl w-80 h-20 text-center">
             AURACAFE
             <div>
               <p>Where coffee meets calm</p>
             </div>
           </div>
-
-          <footer className="flex flex-col mt-auto">Footer bla bla</footer>
         </div>
+
+        <footer className="flex flex-col mt-auto">Footer bla bla</footer>
       </div>
     </div>
   );
